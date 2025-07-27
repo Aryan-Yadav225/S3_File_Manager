bdd53cc523e62bd3f493f0d2cae3ae49b8bb5ce2
Aryan Yadav <aryan225@code@gmail.com>
2025-07-27 13:17:08 +0530

    created route and made changes in config

--- frontend/src/app/apiservice.service.ts
+++ frontend/src/app/apiservice.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiserviceService {
+  private BASE_URL = 'http://localhost:5050';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  addFile(formData: FormData): Observable<any> {
+    return this.http.post(`${this.BASE_URL}/file/upload`, formData);
+  }
+
+  getAllFiles(): Observable<any> {
+    return this.http.get(`${this.BASE_URL}/file/get-all`);
+  }
 }
