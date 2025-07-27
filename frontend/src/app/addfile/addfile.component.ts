926fdcbde72e42d9ea50cb6357de3315717fb09a
Aryan Yadav <aryan225@code@gmail.com>
2025-07-27 13:11:15 +0530

    First Frontend commit

---
+++ frontend/src/app/addfile/addfile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addfile',
+  imports: [],
+  templateUrl: './addfile.component.html',
+  styleUrl: './addfile.component.css'
+})
+export class AddfileComponent {
+
+}
